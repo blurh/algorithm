4f183333e082d66222414691c56cde57dd5a5c85
blurh <blurh_z@163.com>
2021-09-17 18:26:32 +0800

    optimize the insertion sort

--- insertionSort/insertionSort.go
+++ insertionSort/insertionSort.go
@@ -13,13 +13,14 @@ func InsertionSort(lt []int) ([]int, error) {
         return lt, nil
     }
     for offset := 1; offset <= len(lt) - 1; offset++ {
-        for i := 0; i <= offset - 1; i++ {
-            offsetValue := lt[offset]
-            if offsetValue <= lt[i] {
-                lt = append(lt[:offset], lt[offset + 1:]...)
-                lt = append(lt[:i], append([]int{offsetValue}, lt[i:]...)...)
-                break
-            }
+        // 往左移动游标 i, 游标所在值跟 offsetValue 相比较, 将 offsetValue 左移插入到相应的位置
+        // 2, 4, 8, 9, 30, 1, 7, 20, 17, 40
+        //                 ↑
+        //               offset
+        //             ←i
+        offsetValue := lt[offset]
+        for i := offset - 1; i >= 0 && lt[i] > offsetValue; i-- {
+            lt[i], lt[i + 1] = lt[i + 1], lt[i]
         }
     }
     return lt, nil
