11ffd4e0da58d226d6657a172e5c56d590af0cd0
blurh <blurh_z@163.com>
2022-01-22 16:59:49 +0800

    refactor: rename function name BKDRHash to BKDR

--- hashing/bkdr.go
+++ hashing/bkdr.go
@@ -1,6 +1,6 @@
 package hashing
 
-func BKDRHash(str string) uint64 {
+func BKDR(str string) uint64 {
     const seed = uint64(31) // 31 131 1313 13131 131313 etc..
     hash := uint64(0)
     for i := 0; i <= len(str)-1; i++ {
--- hashing/hash_test.go
+++ hashing/hash_test.go
@@ -7,7 +7,7 @@ import (
 func TestHash(t *testing.T) {
     str := "oapfjasasfn"
     t.Run("test for BKDR Hash", func(t *testing.T) {
-        hash := BKDRHash(str)
+        hash := BKDR(str)
         if hash != 1946341454 {
             t.Errorf("BKDR Hash fail")
         }
